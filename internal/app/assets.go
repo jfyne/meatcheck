a76420c1e83bf3568fc30154e0b1d24e04ca4da6
Josh Fyne <josh.fyne@gmail.com>
2026-02-20 09:37:42 +0000

    Fix markdown preview image rendering for local files

--- internal/app/assets.go
+++ internal/app/assets.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"html"
 	"html/template"
+	"net/url"
 	"path/filepath"
 	"strings"
 
@@ -11,6 +12,7 @@ import (
 	"github.com/jfyne/meatcheck/internal/ui"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/extension"
+	xhtml "golang.org/x/net/html"
 )
 
 var (
@@ -34,6 +36,61 @@ func renderMarkdown(input string) template.HTML {
 		return template.HTML(html.EscapeString(input))
 	}
 	return template.HTML(buf.String())
+}
+
+func renderMarkdownDocument(path string, input string) template.HTML {
+	baseDir := filepath.Dir(path)
+	if baseDir == "." {
+		baseDir = ""
+	}
+	rendered := renderMarkdown(input)
+	return rewriteMarkdownImageSources(string(rendered), baseDir)
+}
+
+func rewriteMarkdownImageSources(doc string, baseDir string) template.HTML {
+	root, err := xhtml.Parse(strings.NewReader(doc))
+	if err != nil {
+		return template.HTML(doc)
+	}
+
+	var walk func(*xhtml.Node)
+	walk = func(n *xhtml.Node) {
+		if n.Type == xhtml.ElementNode && n.Data == "img" {
+			for i := range n.Attr {
+				if n.Attr[i].Key != "src" {
+					continue
+				}
+				src := strings.TrimSpace(n.Attr[i].Val)
+				if src == "" || isExternalAssetURL(src) {
+					continue
+				}
+				rel := filepath.Clean(filepath.ToSlash(filepath.Join(baseDir, src)))
+				n.Attr[i].Val = "/file?path=" + url.QueryEscape(rel)
+			}
+		}
+		for child := n.FirstChild; child != nil; child = child.NextSibling {
+			walk(child)
+		}
+	}
+	walk(root)
+
+	var out bytes.Buffer
+	for c := root.FirstChild; c != nil; c = c.NextSibling {
+		if err := xhtml.Render(&out, c); err != nil {
+			return template.HTML(doc)
+		}
+	}
+	return template.HTML(out.String())
+}
+
+func isExternalAssetURL(s string) bool {
+	lower := strings.ToLower(strings.TrimSpace(s))
+	return strings.HasPrefix(lower, "http://") ||
+		strings.HasPrefix(lower, "https://") ||
+		strings.HasPrefix(lower, "data:") ||
+		strings.HasPrefix(lower, "mailto:") ||
+		strings.HasPrefix(lower, "#") ||
+		strings.HasPrefix(lower, "/")
 }
 
 func isMarkdownPath(path string) bool {
